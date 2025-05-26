3e3ab537d451551d28c453e442c10c09d7d83fa6
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-26 01:01:52 +0000

    html, css, js, flowchart, storyboard, picture

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Peter Zerbinos All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Peter Zerbinos
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculateVolumeOfSphere() {
+  // this calculates the volume of a sphere
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radius = params.get('radius')
+  console.log(radius)
+
+  // process
+  const radiusOfSphere = parseFloat(radius)
+  const volumeOfSphere = (4 / 3) * Math.PI * (radiusOfSphere ** 3)
+
+  // output
+  document.getElementById('volume').innerHTML = "Volume is: " + volumeOfSphere.toFixed(2) + "mm³"
+  document.getElementById('radius').innerHTML = radiusOfSphere + "mm"
+}
+
+// On page load, display radius
+window.onload = function() {
+  document.getElementById('radius').innerHTML = 0 + "mm"
+  document.getElementById('volume').innerHTML = "Volume is: " + 0 + "mm³"
+}
